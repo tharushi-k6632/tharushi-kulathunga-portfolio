cad912e95bb3407cd36a3fc0c41993e5992661df
Tharushi <tharushi@gmail.com>
2026-08-16 23:42:08 +0530

    Configure Vite & GitHub Actions deployment for GitHub Pages

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/tharushi-kulathunga-portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+})
